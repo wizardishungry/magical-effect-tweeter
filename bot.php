fa90e96e7bdc4b6f7e0a5ea778db22aff61d6b5c
Jon Williams <jonathan.williams@gmail.com>
2011-08-24 14:30:32 -0400

    gold teef

--- bot.php
+++ bot.php
@@ -8,11 +8,21 @@ define('ACCESS_TOKEN_SECRET', 'insert_your_access_token_secret_here');
 
 $twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 $twitter->host = "http://search.twitter.com/";
-$search = $twitter->get('search', array('q' => 'semoga', 'rpp' => 20));
+$ron = $twitter->get('search', array('q' => 'ron paul', 'rpp' => 20));
+$gucci = $twitter->get('search', array('q' => 'gucci mane', 'rpp' => 20));
 
 $twitter->host = "https://api.twitter.com/1/";
-foreach($search->results as $tweet) {
-	$status = 'Amiiiin RT @'.$tweet->from_user.' '.$tweet->text;
-	if(strlen($status) > 140) $status = substr($status, 0, 139);
-	$twitter->post('statuses/update', array('status' => $status));
+$tweets = array_merge($ron->results,$gucci->results);
+shuffle($tweets);
+foreach($tweets as $tweet) {
+  $txt=preg_replace(Array('/Ron Paul/i','/Gucci Mane/i'), Array('GMGMGMGM', 'RPRPRPRP'), $tweet->text);
+  $txt=preg_replace(Array('/GMGMGMGM/', '/RPRPRPRP/'), Array('Gucci Mane', 'Ron Paul'), $txt);
+	$status = 'RT @'.$tweet->from_user.' '.$txt;
+  if( strlen($status)<=140 && !preg_match('/librrrtarian/i',$status)&&!preg_match(array('/RT /i','/#np/i', $txt)&&!preg_match('/bot/i',$status))
+  {
+      $twitter->post('statuses/update', array('status' => $status));
+    //echo $tweet->text,"\n";
+    echo $status,"\n";
+    sleep(rand(60,120));
+  }
 }
