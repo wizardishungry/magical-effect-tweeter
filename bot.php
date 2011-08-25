f19615b8826693e2ab52104b47147ed4a10fa6a1
Jon Williams <jonathan.williams@gmail.com>
2011-08-25 14:49:22 -0400

    statew

--- bot.php
+++ bot.php
@@ -18,6 +18,9 @@ foreach($tweets as $tweet) {
   $txt=preg_replace(Array('/Ron Paul/i','/Gucci Mane/i'), Array('GMGMGMGM', 'RPRPRPRP'), $tweet->text);
   $txt=preg_replace(Array('/GMGMGMGM/', '/RPRPRPRP/'), Array('Gucci Mane', 'Ron Paul'), $txt);
 	$status = 'RT @'.$tweet->from_user.' '.$txt;
+  if(preg_match('/@yeahok/',$status)) {
+    $status=preg_replace(Array('/Ron Paul/i', '/Gucci Mane/i','/GucciDoinThings/i','/@gucci1017/i',), Array('state', 'state','yeahok','@yeahok','@yeahok'), $status);
+  }
   if( strlen($status)<=140 && !preg_match('/librrrtarian/i',$status)
     &&!preg_match('/Now Playing/i',$txt)&&!preg_match('/NowPlaying/i',$txt)
     &&!preg_match('/RT /i',$txt) &&!preg_match('/[ #]np/i', $txt)&&!preg_match('/bot/i',$status))
