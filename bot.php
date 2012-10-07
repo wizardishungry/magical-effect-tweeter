92a3f73cd61867e1c7a121263b3972c7a774dde9
Jon Williams <jon.williams@hollywood.com>
2012-10-07 03:00:15 -0400

    load words from txts

--- bot.php
+++ bot.php
@@ -4,66 +4,17 @@ require_once('config.php');
 require_once('magic.php');
 require_once('lib.php');
 
-$bad_words = array(
-  'video',
-  'ft.',
-  'feat[. ]',
-  'playin',
-  'np',
-  '^RT',
-  '^@',
-  'http',
-  'via @',
-  'RT @',
-  ':',
-  '@',
-  'stolas_real',
-  'prince_stolas',
-);
+$path = dirname(__FILE__);
 
-$searches = array(
-    'stolas',
-    'lucifer',
-    'satan',
-    'horoscope',
-    'prana',
-    'yoga',
-    'astrology',
-    'namaste',
-    'jung',
-    'freud',
-    'vinyasa',
-    'soul',
-    'concious',
-    'pray',
-    'jesus',
-    'allah',
-    'genie',
-    'djinn',
-    'cosmic',
-    'astral',
-    'eternal',
-    'god',
-    'billion',
-    "scorpio",
-    "aquarius",
-    "gemini",
-    "virgo",
-    "aries",
-    "pisces",
-    "sagittarius",
-    "libra",
-    "witch",
-    "witch house",
-    "leo",
-);
+$flags=FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES;
+$searches  = file("$path/whitelist.txt",$flags);
+$bad_words = file("$path/blacklist.txt",$flags);
 
 $user_wait_time = 86400; // time before responding to user again
 
 /////////////////////////////////////////////////////////////////////////////////////////
 date_default_timezone_set('America/New_York');
 
-$path = dirname(__FILE__);
 $state = json_decode(@file_get_contents("$path/STATE"), true);
 if(!$state) {
     $state = array(
@@ -81,6 +32,8 @@ $twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_
 
 $twitter->host = "https://api.twitter.com/1/";
 $tweets = $twitter->get('statuses/friends_timeline',array('count' => 140));
+//$tweets = $twitter->get('statuses/mentions',array('include_rts' => false));
+
 
 $tweets = array_filter($tweets, function($tweet) {
     global $bad_words, $searches;
@@ -101,14 +54,15 @@ $tweets = array_filter($tweets, function($tweet) {
     $time = strtotime($tweet->created_at);
 
     if($tweet->retweeted) $tweet->score-=20;
-    if($tweet->in_reply_to_screen_name) $tweet->score-=10;
+    if(!$tweet->user->following) $tweet->score-=250;
+    if($tweet->in_reply_to_screen_name) $tweet->score-=100;
     $tweet->score+=300*palindrome($tweet->text);
     $tweet->score+=3*min(30,$tweet->retweet_count);
     $tweet->score+=50*($tweet->text==strtoupper($tweet->text));
     $tweet->score+=70*($tweet->text==strtolower($tweet->text));
     $tweet->score+=100*(strtoupper($tweet->text)==strtolower($tweet->text));
     $tweet->score+=10*preg_match('/  /',$tweet->text);
-    $tweet->score+=80*preg_match('/stolas/i',$tweet->in_reply_to_screen_name);
+    $tweet->score+=800*preg_match('/stolas/i',$tweet->in_reply_to_screen_name);
     $tweet->score-=180*preg_match('/stolas/i',$tweet->user->screen_name);
     $tweet->score+=40*preg_match('/\xEE[\x80-\xBF][\x80-\xBF]|\xEF[\x81-\x83][\x80-\xBF]/', $tweet->text);
     $tweet->score+=30*($tweet->source!='web');
@@ -116,24 +70,27 @@ $tweets = array_filter($tweets, function($tweet) {
     $tweet->score+=0.03*min(1000,$tweet->user->favourites_count);
     $tweet->score-=20*$tweet->user->verified;
 
-    $ok = false;
+    $ok = 0;
 
     foreach($searches as $word) {
         $flag = (($word==strtolower($word))?"i":'');
         if(preg_match("/$word/$flag",$tweet->text) != false) {
             //echo "ACCEPT($word): ", $tweet->text,"\n";
-            $tweet->score+=150;
-            $ok=true;
+            $tweet->score+=$ok?50:200;
+            if($ok>4) {
+                echo "stop gaming\n";
+                print_r($tweet);
+                return false;
+            }
+            $ok++;
         }
     }
 
 
-    return $ok;
+    return $ok>0;
     return $tweet->score>0;
 });
 
-//print_r($tweets);
-//exit;
 
 $used=array();
 $time_parts=localtime(time(),true);
@@ -149,12 +106,13 @@ foreach($tweets as $tweet) {
     $consider[$user]=max($time,@$consider[$user]);
 
     $difficulty = 1000
-        + 250  *( $yes&&$allowed ) // be less likely with sequential tweets
-        - 200  *( time() - $state['time'] > $user_wait_time ) // be more likely if we havent succeeded in a while
-        - 150  *( time() - $time <= 60 ) // be more likely if the tweet was in last 60 seconds
-        - 100  *( time() - $time <= 300 ) // be more likely if the tweet was in last 5 min
-        + 250  *( !in_array($time_parts['tm_wday'],array(0,6)) && in_array($time_parts['tm_hour'],range(6,20)) ) // be more difficult during work week
-        - 300  *( in_array($time_parts['tm_hour'],range(0,4)) ) // more late night
+        + 2250 *( !@$state['consider'][$user] ) // be much less likely with new tweeters
+        +  250 *( $yes&&$allowed ) // be less likely with sequential tweets
+        -  200 *( time() - $state['time'] > $user_wait_time ) // be more likely if we havent succeeded in a while
+        -  150 *( time() - $time <= 60 ) // be more likely if the tweet was in last 60 seconds
+        -  100 *( time() - $time <= 300 ) // be more likely if the tweet was in last 5 min
+        +  550 *( !in_array($time_parts['tm_wday'],array(0,6)) && in_array($time_parts['tm_hour'],range(6,20)) ) // be more difficult during work week
+        -  350 *( in_array($time_parts['tm_hour'],range(0,4)) ) // more late night
     ;
     $yes=$tweet->score > rand(0,$difficulty);
 
